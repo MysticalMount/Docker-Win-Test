45d161e3f1835aaaed526e20e6a101eede7d0522
James <jjjwils@gmail.com>
2018-07-19 16:05:21 +0100

    Initial commit

---
+++ Nano-IIS/Deploy.ps1
@@ -0,0 +1,17 @@
+﻿# Deploy IIS to Nano Server using pre-downloaded Nano server packages
+
+Start-Transcript -Path C:\Deploy\Deploy.Log
+
+$IISPackage = ("C:\Deploy\NanoIIS1.cab", "C:\Deploy\NanoIIS2.cab")
+
+Write-Output "Enabling IIS-Webserver"
+
+ForEach($PackageString in $IISPackage)
+{
+	Write-Output $PackageString
+	
+	Add-WindowsPackage -Online -PackagePath $PackageString
+	Add-WindowsPackage -Online -PackagePath $PackageString
+}
+
+Stop-Transcript
